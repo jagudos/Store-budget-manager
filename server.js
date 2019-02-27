0cabbec47d25fa885c3feb5670267a55734d5549
angelperea <aperea.z29@gmail.com>
2019-02-27 07:09:41 +0100

    Test inicial - cambiado puerto al 81 y al 444 para https

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ process.on('SIGINT', function () {
 //var server = app.listen(80, function () {
 //    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 //});
-app.listen(80);
-https.createServer(options, app).listen(443);
+app.listen(81);
+https.createServer(options, app).listen(444);
 
 
